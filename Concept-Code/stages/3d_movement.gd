be20f5c5ff715ec1a7de715690e4ddf6f540cc7c
mitchp819 <135446256+mitchp819@users.noreply.github.com>
2024-05-14 14:19:07 -0400

    3D works well

---
+++ Concept-Code/stages/3d_movement.gd
@@ -0,0 +1,12 @@
+extends Node3D
+var speed = 3
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	$buildingsChunk.position += Vector3(0,0,delta*speed)
+	pass
